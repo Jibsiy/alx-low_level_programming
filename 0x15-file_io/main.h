c9c639e14ddfe6b26586d3320781783bbab40cca
jibsiy <jibriy381@gmail.com>
2023-11-27 06:33:16 -0800

    first

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+#include <unistd.h>
+
+Ssize_t read_textfile(const char*filename,size_letters, size_t letters);
+Int create_file(const char*text_content);
+Int append_text_to_file(const char*filename, char* text_content);
+#endif
+
