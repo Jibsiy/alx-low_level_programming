b70e05b8a9d287923cc77c994c7bd7950e13c9ce
jibsiy <jibriy381@gmail.com>
2023-11-29 05:17:18 -0800

    when

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <fcnt1.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 Ssize_t read_textfile(const char*filename,size_letters, size_t letters);
 Int create_file(const char*text_content);
 Int append_text_to_file(const char*filename, char* text_content);
