d6ff60095f7f5fa55f3801378c68ee5125a47b42
Jibsiy <117862450+Jibsiy@users.noreply.github.com>
2023-11-30 14:48:55 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdisb.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcnt1.h>
 #include <unistd.h>
-#include <stdlib.h>
+
+
+
+
 Ssize_t read_textfile(const char*filename,size_letters, size_t letters);
 Int create_file(const char*text_content);
 Int append_text_to_file(const char*filename, char* text_content);
